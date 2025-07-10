517de36e4fbbfd65c8c566d57279cfae9d3a7d80
Мартышов Данила Викторович <409091@niuitmo.ru>
2025-07-10 14:49:53 +0300

    Changes to servlet architecture; separated servlet into servlet and service; changed tests accordingly

--- src/main/java/com/example/t1_hw/SupportServlet.java
+++ src/main/java/com/example/t1_hw/SupportServlet.java
@@ -7,32 +7,36 @@ import java.util.Random;
 import jakarta.servlet.http.*;
 
 public class SupportServlet extends HttpServlet {
-    private ArrayList<String> helpMessages;
-    private Random random;
+    private SupportService supportService;
 
+    @Override
     public void init() {
-        helpMessages = new ArrayList<>();
-        helpMessages.add("У тебя все получится");
-        helpMessages.add("Выше нос!");
-        random = new Random();
+        supportService = new SupportService();
+    }
+    public SupportServlet(SupportService service){
+        this.supportService = service;
     }
 
+    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/plain");
         PrintWriter out = response.getWriter();
-        int randomIndex = random.nextInt(helpMessages.size());
-        out.println(helpMessages.get(randomIndex));
+        String message = supportService.getRandomMessage();
+        out.append(message);
     }
-    public void doPost(HttpServletRequest request, HttpServletResponse response){
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
         try(BufferedReader in = request.getReader()){
             String message = in.readLine();
-            helpMessages.add(message);
+            supportService.addMessage(message);
+            out.append(message);
         } catch (IOException e){
             response.setStatus(500);
+            out.append("Error:").append(e.getMessage());
         }
-    }
-    public ArrayList<String> getHelpMessages(){
-        return this.helpMessages;
+
     }
 
 }
--- src/test/java/SupportServletTest.java
+++ src/test/java/SupportServletTest.java
@@ -1,3 +1,4 @@
+import com.example.t1_hw.SupportService;
 import com.example.t1_hw.SupportServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -5,6 +6,7 @@ import org.junit.jupiter.api.*;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.logging.Logger;
 
 import static org.mockito.Mockito.*;
 
@@ -12,34 +14,43 @@ public class SupportServletTest {
     private SupportServlet servlet;
     private HttpServletRequest request;
     private HttpServletResponse response;
+    private SupportService service;
     public Writer writer;
     public Reader reader;
+    private static final Logger logger = Logger.getLogger("Tests");
+    private final String testRandomMessage = "У тебя все получится";
 
     @BeforeEach
     public void initTest() throws IOException {
-        servlet = new SupportServlet();
-        servlet.init();
+        service = mock(SupportService.class);
+        when(service.getRandomMessage()).thenReturn(testRandomMessage);
+        servlet = new SupportServlet(service);
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
         writer = new StringWriter();
         when(response.getWriter()).thenReturn(new PrintWriter(writer));
     }
+
     @Test
     public void testDoGet() throws IOException{
         servlet.doGet(request, response);
         PrintWriter resultWriter = response.getWriter();
         resultWriter.flush();
         String result = writer.toString().trim();
-        ArrayList<String> messages = servlet.getHelpMessages();
-        Assumptions.assumeTrue(messages.contains(result));
+        logger.info(result);
+        Assertions.assertEquals(testRandomMessage, result);
     }
+
     @Test
     public void testDoPost() throws IOException{
         String testMessage = "я в тебя верю!";
         reader = new StringReader(testMessage);
         when(request.getReader()).thenReturn(new BufferedReader(reader));
         servlet.doPost(request, response);
-        ArrayList<String> resultMessages = servlet.getHelpMessages();
-        Assumptions.assumeTrue(resultMessages.contains(testMessage));
+        PrintWriter resultWriter = response.getWriter();
+        resultWriter.flush();
+        String result = writer.toString().trim();
+        logger.info(result);
+        Assertions.assertEquals(testMessage, result);
     }
 }
